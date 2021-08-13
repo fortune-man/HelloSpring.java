30d3defa2fc5ca92cb386f4d54b5ac8db77405d4
HelloWorld-jh <79819812+HelloWorld-jh@users.noreply.github.com>
2021-08-13 11:28:44 +0900

    Add files via upload

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,46 @@
+package hello.hellospring;
+import hello.hellospring.aop.TimeTraceApp;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+@Configuration
+public class SpringConfig {
+
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+    }
+
+
+    public TimeTraceApp timeTraceApp() {
+        return new TimeTraceApp();
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//// return new MemoryMemberRepository();
+//// return new JdbcMemberRepository(dataSource);
+//// return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//
+//    }
+}
